f94ebd6757352ac5ad3183eddc81047a7f96ffff
Armaan A <49625265+Armster15@users.noreply.github.com>
2022-02-20 15:07:31 -0800

    remove commented dep

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
-// import webWorkerLoader from 'rollup-plugin-web-worker-loader';
 import comlink from 'vite-plugin-comlink'
 import worker, { pluginHelper } from 'vite-plugin-worker'
 
